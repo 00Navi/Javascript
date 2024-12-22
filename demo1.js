cce07432139a92677bbd6825fc50bfc59a7fd6ca
00Navi <en21cs301497@medicaps.ac.in>
2024-12-22 09:41:12 +0530

    First commit

---
+++ demo1.js
@@ -0,0 +1,47 @@
+// let score= "True"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let SomeNumber = 84
+// let stringNumber = String(SomeNumber)
+// console.log(typeof stringNumber )
+
+let value=45
+let negValue=-value
+console.log(negValue);
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+// console.log("05">2)
+// console.log("5"===5);
+
+const id=Symbol("100")
+const anotherId=Symbol("100")
+console.log(id===anotherId)
+
+const name="Rohit";
+const repoCount=4;
+
+console.log(`My name is ${name} and I have ${repoCount} repos`);
+
+console.log(name.toUpperCase());
+console.log(name.charAt(3));
+console.log(name.indexOf('t'));
+
+const url="https://www.xyz$45.com"
+
+console.log(url.replace('$45','travel'))
+
+
+
+
+
+
+
+
